0c19290b2697d495b1785569bac4918c8e6aee80
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-17 16:49:32 +0200

    img su JS

--- JS/main.js
+++ JS/main.js
@@ -14,7 +14,23 @@ console.log(rowEl)
 fetch(endpoint)
     .then(response => response.json())
     .then(data => {
+        data.forEach((img) => {
+            const { url } = img
+            console.log(url);
 
+            const imgMarkUp = `
+             <div class="row">
+                <div class="col">
+                    <div class="card">
+                      <img src="${url}">
+                      <img class="pin" src="./img/pin.svg">
+                    </div>
+                </div>
+            </div>
+            `
+            rowEl.insertAdjacentHTML("beforeend", imgMarkUp)
+
+        })
         console.log(data)
 
     }).catch(error => {
