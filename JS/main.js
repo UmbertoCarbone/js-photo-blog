bcd352e99f6194becf504e33e81e33211ea80f2c
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-17 15:19:20 +0200

    dsds

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,24 @@
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
-console.log(endpoint)
+const cardEl = document.getElementById("card")
+console.log(cardEl)
+
+const colEl = document.getElementById("col")
+console.log(colEl)
+
+const rowEl = document.getElementById("row")
+console.log(rowEl)
+
+
 
 fetch(endpoint)
     .then(response => response.json())
     .then(data => {
         console.log(data)
 
+
     }).catch(error => {
         console.error(error);
     });
+
 
