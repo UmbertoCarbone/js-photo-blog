e8b7cefa8a88faf0171b98dbb5df36c42c2e0abb
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-18 11:17:05 +0200

    overlay html + css

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,5 @@
 const endpoint = "https://lanciweb.github.io/demo/api/pictures/"
 
-const cardEl = document.querySelector(".card")
-console.log(cardEl)
-
-const colEl = document.querySelector(".col")
-console.log(colEl)
 
 const rowEl = document.querySelector(".row")
 console.log(rowEl)
@@ -36,5 +31,7 @@ fetch(endpoint)
     }).catch(error => {
         console.error(error);
     });
+
+
 
 
