70f72d92a18eeb447448afb8dd67cc8aa8509f57
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-18 17:48:27 +0200

    aggiunta funzionalità di overlay per visualizzare l'immagine selezionata e migliorata la struttura del codice

--- JS/main.js
+++ JS/main.js
@@ -7,6 +7,8 @@ console.log(rowEl)
 const overlayEl = document.querySelector(".overlay")
 console.log(overlayEl)
 
+const imgOverlayEl = overlayEl.querySelector("img")
+
 const overlayContentEl = document.querySelector(".overlayContent")
 console.log(overlayContentEl)
 
@@ -16,14 +18,24 @@ console.log(closeBtnEl)
 fetch(endpoint)
     .then(response => response.json())
     .then(data => {
-        data.forEach((img) => {
-            let { title, url, date } = img
+        data.forEach((post) => {
+            const { title, url, date } = post
             console.log(title, url, date);
+            /*  const title = img.title
+             const url = img.url
+             const date = img.date */
+
+
+
+            // const card = document.createElement('div') // <div></div>
+            // card.classList.add('card')
+            // card.addEventListener(....)
+            // rowEl.appendChild(card)
 
             const imgMarkUp = `
                 <div class="col-sm col-md col-lg col-xl ">
                     <div class="card">
-                    <h2>${title}</h2>
+                        <h2>${title}</h2>
                       <img src="${url}">
                       <img class="pin" src="./img/pin.svg">
                       <h2>${date}</h2>
@@ -33,33 +45,64 @@ fetch(endpoint)
             rowEl.insertAdjacentHTML("beforeend", imgMarkUp)
 
         })
-        console.log(data)
+
+        const cardsEl = document.querySelectorAll(".card")
+        /* console.log(cardsEl) */
+        cardsEl.forEach((card) => {
+            card.addEventListener('click', () => {
+                /* console.log(card); */
+                const cardImg = card.querySelector('img')
+                const cartTitle = card.querySelector('h2')
+                console.log(cartTitle.innerText);
+
+
+                /* console.log(cardImg); */
+                imgOverlayEl.src = cardImg.src
+
+                //     overlayContentEl.innerHTML = `
+
+                //     <img src="${cardImgUrl}" style="width: 500px">
+                // `;
+                /*  console.log(cardImgUrl) */
+                overlayEl.style.display = 'flex';
+                /* const cardImgUrl = cardImg.src
+                console.log(cardImgUrl); */
+            })
+        })
+
+        closeBtnEl.addEventListener('click', () => {
+            overlayEl.style.display = 'none';
+        });
 
     }).catch(error => {
         console.error(error);
     });
 
+// qui quando clicclo sul buttom, torna alla pagina principale
+
+
 //qui premo la card e mi esce lo sfondo ombrato con l'img selezionata
-rowEl.addEventListener("click", () => {
+/* rowEl.addEventListener("click", () => {
 
-    overlayEl.style.display = 'flex';
-    //NON FUNZIONA
+    overlayEl.style.display = 'flex'; */
+//     //NON FUNZIONA
 
-    //uso template literal 
-    const imgMark = ` <div class="col-sm col-md col-lg col-xl ">
-                    <div class="card">
-                    <h2>${title}</h2>
-                      <img src="${url}">
-                      <h2>${date}</h2>
-                    </div>
-                </div> `
-    overlayContentEl.insertAdjacentHTML("beforeend", imgMark)
+//uso template literal
+/*   const imgMark = ` <div class="col-sm col-md col-lg col-xl ">
+                   <div class="card">
+                  <h2>${title}</h2>
+                     <img src="${url}">
+                     <h2>${date}</h2>
+                   </div>
+               </div> `
 
-});
+
+}); */
 
 // qui quando clicclo sul buttom, torna alla pagina principale
-closeBtnEl.addEventListener('click', () => {
+/* closeBtnEl.addEventListener('click', () => {
     overlayEl.style.display = 'none';
 });
 
 
+ */
